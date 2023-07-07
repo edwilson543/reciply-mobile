90a44b30087eb2dfc57cbd7dd634ab623a165936
edwilson <edward.e.j.wilson@gmail.com>
2023-07-07 09:10:07 +0100

    Make recipe list fetching more hooky

--- App/screens/recipes/MyRecipeList/MyRecipeList.tsx
+++ App/screens/recipes/MyRecipeList/MyRecipeList.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import {useEffect, useState} from 'react';
 
 import {MyRecipeListProps} from '../../../navigation/navigation.types';
 import MyRecipeListView from './MyRecipeListView';
-import {RecipePreview} from '../../../utils/types/recipes';
-import {getMyRecipeList} from '../../../services/restAPI/recipes';
+import {useMyRecipeList} from '../../../services/restAPI/recipes';
 
 export function MyRecipeList({navigation}: MyRecipeListProps) {
   /** Container for the screen showing the user's recipes. */
-  const [recipeList, setRecipeList] = useState<Array<RecipePreview>>([]);
-
-  useEffect(() => {
-    getMyRecipeList().then(data => setRecipeList(data));
-  }, []);
+  const recipeList = useMyRecipeList();
 
   return <MyRecipeListView recipes={recipeList} navigation={navigation} />;
 }
--- App/services/restAPI/recipes.ts
+++ App/services/restAPI/recipes.ts
@@ -1,8 +1,22 @@
 import {RecipePreview} from '../../utils/types/recipes';
 import data from './__mocks__/data/MyRecipeList.json';
 
-export async function getMyRecipeList(): Promise<Array<RecipePreview>> {
+import {useEffect, useState} from 'react';
+
+export function useMyRecipeList(): Array<RecipePreview> {
+  /** Fetch the recipe list and return it */
+  const [recipeList, setRecipeList] = useState<Array<RecipePreview>>([]);
+
+  useEffect(() => {
+    getMyRecipeList().then(responseData => setRecipeList(responseData));
+  }, []);
+
+  return recipeList;
+}
+
+async function getMyRecipeList(): Promise<Array<RecipePreview>> {
   // TODO -> actually make an API request and relegate this implementation to the mock.
+  // This can be some generic hook
   return new Promise(function (resolve, reject) {
     resolve(data);
     reject();
