cf6d8316394b32ea2154264c9bec53818c228f27
edwilson <edward.e.j.wilson@gmail.com>
2023-07-08 10:28:25 +0100

    Set the auth token in storage once received from the API

--- App/screens/auth/SignIn/Login.tsx
+++ App/screens/auth/SignIn/Login.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import {useState} from 'react';
+import React, {useState} from 'react';
 
 import LoginView from './LoginView';
 import {login} from '../../../services/restAPI/authRequests';
 import * as exceptions from '../../../services/restAPI/exceptions';
 import * as auth from '../../../context/auth';
+import * as storage from '../../../services/storage';
+import {StorageKey} from '../../../services/storage';
 
 export function Login() {
   /** Authenticate users using their username and password. */
@@ -15,15 +16,16 @@ export function Login() {
   const canSubmit = username.length > 2 && password.length > 2;
   const authDispatch = auth.useAuthDispatch();
 
-  // TODO -> make a submit button
   function handleLogin(): void {
     login(username, password)
-      .then(data =>
+      .then(data => {
+        // TODO -> is it a bad practice to have the token in storage AND context
+        storage.setValueForKey(StorageKey.AuthToken, data.token);
         authDispatch({
           type: auth.AuthAction.Login,
           token: data.token,
-        }),
-      )
+        });
+      })
       .catch(error => {
         if (error instanceof exceptions.UnauthorizedError) {
           setErrorMessage('Invalid username or password');
--- App/services/restAPI/constants.ts
+++ App/services/restAPI/constants.ts
@@ -1,4 +1,4 @@
-export const APILocation = 'http://127.0.0.1:8001/api/';
+export const APILocation = 'http://127.0.0.1:8000/api/';
 
 export enum StatusCode {
   // Client error responses
