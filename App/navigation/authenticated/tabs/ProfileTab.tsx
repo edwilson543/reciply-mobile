2e771d8330212450a9870212b726c8428404feb8
edwilson <edward.e.j.wilson@gmail.com>
2023-07-10 07:33:05 +0100

    Add a profile tab

---
+++ App/navigation/authenticated/tabs/ProfileTab.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import * as profileScreens from '../../../screens/profile';
+import {useColourScheme} from '../../../styles/colourScheme';
+import {headerScreenStyles} from '../../../styles/navigation';
+import {ScreenName} from '../../constants';
+import HeaderTitle from '../components/HeaderTitle';
+import {ProfileStackParamsList} from '../navigation.types';
+
+const ProfileStack = createNativeStackNavigator<ProfileStackParamsList>();
+
+export function ProfileTab() {
+  const colourScheme = useColourScheme();
+
+  return (
+    <ProfileStack.Navigator
+      screenOptions={{
+        headerTitle: HeaderTitle,
+        ...headerScreenStyles(colourScheme),
+      }}>
+      <ProfileStack.Screen
+        name={ScreenName.Account}
+        component={profileScreens.Account}
+      />
+    </ProfileStack.Navigator>
+  );
+}
---
+++ App/screens/profile/Account/Account.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Pressable, ScrollView, Text} from 'react-native';
+
+export function Account() {
+  return (
+    <ScrollView>
+      <Pressable>
+        <Text>Logout</Text>
+      </Pressable>
+    </ScrollView>
+  );
+}
