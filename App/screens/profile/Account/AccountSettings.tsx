52826f141a506a2e64c002b594c61dc551b095fa
edwilson <edward.e.j.wilson@gmail.com>
2023-07-10 17:44:16 +0100

    Split the logout button into its own component

--- App/screens/profile/Account/AccountSettings.tsx
+++ App/screens/profile/Account/AccountSettings.tsx
@@ -1,30 +1,15 @@
 import React from 'react';
 
-import {Pressable, ScrollView, Text} from 'react-native';
+import {ScrollView} from 'react-native';
 
-import * as auth from '../../../context/auth';
-import {logout} from '../../../services/restAPI/authRequests/logout';
-import * as storage from '../../../services/storage';
-import {StorageKey} from '../../../services/storage';
+import LogoutButton from './LogoutButton';
 
 export function AccountSettings() {
   /** View of the user's accounts with links to relevant actions. */
-  const authDispatch = auth.useAuthDispatch();
-
-  function handleLogout(): void {
-    logout().then(() => {
-      storage.deleteValueForKey(StorageKey.AuthToken);
-      authDispatch({
-        type: auth.AuthAction.Logout,
-      });
-    });
-  }
 
   return (
     <ScrollView>
-      <Pressable onPress={handleLogout}>
-        <Text>Logout</Text>
-      </Pressable>
+      <LogoutButton />
     </ScrollView>
   );
 }
