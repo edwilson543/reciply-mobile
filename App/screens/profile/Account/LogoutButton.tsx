b6d6fee865054d981e15367294d0ac55486e9e58
edwilson <edward.e.j.wilson@gmail.com>
2023-07-10 17:54:26 +0100

    Basic basic styling of logout button

--- App/screens/profile/Account/LogoutButton.tsx
+++ App/screens/profile/Account/LogoutButton.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
-import {Pressable, Text} from 'react-native';
+import {Pressable, StyleSheet, Text} from 'react-native';
 
 import * as auth from '../../../context/auth';
 import {logout} from '../../../services/restAPI/authRequests/logout';
 import * as storage from '../../../services/storage';
 import {StorageKey} from '../../../services/storage';
+import {ColourScheme, useColourScheme} from '../../../styles/colourScheme';
+import {FontSize} from '../../../styles/constants';
 
 export default function LogoutButton() {
   /** Button for logging users out. */
-
+  const colourScheme = useColourScheme();
+  const styleSheet = styles(colourScheme);
   const authDispatch = auth.useAuthDispatch();
 
   function handleLogout(): void {
@@ -22,8 +25,26 @@ export default function LogoutButton() {
   }
 
   return (
-    <Pressable onPress={handleLogout}>
-      <Text>Logout</Text>
+    <Pressable onPress={handleLogout} style={styleSheet.settingRow}>
+      <Text style={styleSheet.settingText}>Logout</Text>
     </Pressable>
   );
 }
+
+const styles = (colourScheme: ColourScheme) =>
+  StyleSheet.create({
+    settingRow: {
+      // Display
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      padding: 20,
+      // Background and border
+      backgroundColor: colourScheme.buttonPrimary,
+    },
+    settingText: {
+      // Typography
+      fontSize: FontSize.Text,
+      fontWeight: 'bold',
+      color: colourScheme.buttonPrimaryFont,
+    },
+  });
