7c96c154f9ff706df52b90e516de7952036ce685
edwilson <edward.e.j.wilson@gmail.com>
2023-07-11 08:57:10 +0100

    Add alert background and font colours to the colour scheme

--- App/styles/colourScheme/ColourSchemeLight.ts
+++ App/styles/colourScheme/ColourSchemeLight.ts
@@ -1,19 +1,31 @@
 import {ColourScheme} from './ColourScheme.type';
 
 export default class ColourSchemeLight implements ColourScheme {
-  // Background colours
+  // Background
   backgroundPrimary = '#fff';
   backgroundSecondary = '#ace6ce';
 
-  // Button colours
+  // Fonts
+  fontPrimary = '#000000';
+  fontSecondary = '#fff';
+
+  // Buttons
   buttonPrimary = '#7c8be3';
   buttonSecondary = '#e76c6c';
 
-  // Button font colours
+  // Button fonts
   buttonPrimaryFont = '#fff';
   buttonSecondaryFont = '#fff';
 
-  // Font colours
-  fontPrimary = '#000000';
-  fontSecondary = '#fff';
+  // Alerts
+  alertPrimary = '#7e9ece';
+  alertSuccess = '#6fd7a7';
+  alertWarning = '#e3cf91';
+  alertDanger = '#e5939b';
+
+  // Alert fonts
+  alertPrimaryFont = '#0d6efd';
+  alertSuccessFont = '#198754';
+  alertWarningFont = '#ffc107';
+  alertDangerFont = '#dc3545';
 }
