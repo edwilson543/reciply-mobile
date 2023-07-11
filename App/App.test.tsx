8f906f41e24d209e5fa41a4b9eb7fc34f9008239
edwilson <edward.e.j.wilson@gmail.com>
2023-07-11 17:50:04 +0100

    Add `App` component test for when there is and isn't a token available in storage

--- App/App.test.tsx
+++ App/App.test.tsx
@@ -1,10 +1,25 @@
 import 'react-native';
 import React from 'react';
 
-import {render} from '@testing-library/react-native';
+import {act, render, screen} from '@testing-library/react-native';
 
 import App from './App';
+import * as storage from './services/storage';
 
-test('app renders', () => {
+test('initially renders login screen when no auth token in storage', () => {
+  storage.deleteValueForKey(storage.StorageKey.AuthToken);
+
   render(<App />);
+
+  expect(screen.getByText('Username')).toBeOnTheScreen();
+});
+
+test('initially renders recipe list when auth token available', async () => {
+  storage.setValueForKey(storage.StorageKey.AuthToken, 'dummy-token');
+
+  render(<App />);
+
+  await act(() => {
+    expect(screen.getByText('My recipes')).toBeOnTheScreen();
+  });
 });
