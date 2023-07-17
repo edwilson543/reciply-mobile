cac8b60b74d620113ebed60540eee6e784e4e0f8
edwilson <edward.e.j.wilson@gmail.com>
2023-07-17 07:50:54 +0100

    Add unit test for create recipe screen

---
+++ App/screens/recipes/CreateRecipe/CreateRecipe.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {fireEvent, render, screen} from '@testing-library/react-native';
+
+import {CreateRecipe} from './CreateRecipe';
+import {postData} from '../../../services/restAPI/request';
+
+jest.mock('../../../services/restAPI/request');
+
+test('cannot submit recipe if name given is too short', async () => {
+  jest.mocked(postData).mockResolvedValueOnce(new Response('{}'));
+
+  render(
+    <CreateRecipe navigation={jest.fn() as any} route={jest.fn() as any} />,
+  );
+
+  // Input a name that is too short
+  const nameInput = screen.getByTestId('name-input');
+  fireEvent.changeText(nameInput, 'sa');
+
+  // Confirm new recipe
+  const submitButton = screen.getByTestId('submit-button');
+  expect(submitButton).toBeDisabled();
+});
