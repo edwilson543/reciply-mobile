4fd9a3eb5024b76a73bd0474029a9f6cd6b12d0e
edwilson <edward.e.j.wilson@gmail.com>
2023-07-31 20:10:58 +0100

    Add unit test for the my recipe list view

---
+++ App/screens/recipes/MyRecipeList/MyRecipeList.test.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import {within, screen, render, waitFor} from '@testing-library/react-native';
+
+import {MyRecipeList} from './MyRecipeList';
+import {RecipeListPayload} from '../../../services/restAPI/payloads';
+import {useMyRecipeList} from '../../../services/restAPI/requests/recipes';
+
+jest.mock('../../../services/restAPI/requests/recipes');
+
+test('renders recipe list', async () => {
+  const recipes = [];
+  for (let i = 0; i < 10; i++) {
+    recipes.push({
+      id: 1,
+      name: 'sausages',
+      description: '',
+    } as RecipeListPayload);
+  }
+  const mockRecipeList = {
+    data: [
+      {id: 1, name: 'sausages', description: ''} as RecipeListPayload,
+      {id: 2, name: 'meatballs', description: ''} as RecipeListPayload,
+    ],
+    friendlyErrors: null,
+    isLoading: false as false,
+  };
+  jest.mocked(useMyRecipeList).mockReturnValue(mockRecipeList);
+
+  await waitFor(() =>
+    render(
+      <MyRecipeList navigation={jest.fn() as any} route={jest.fn() as any} />,
+    ),
+  );
+
+  expect(jest.mocked(useMyRecipeList).mock.calls).toHaveLength(1);
+
+  // Both recipes should be shown
+  expect(screen.getByTestId('recipes-header')).toBeOnTheScreen();
+  const sausageRecipe = screen.getByTestId('recipe-1');
+  expect(within(sausageRecipe).getByText('sausages')).toBeVisible();
+  const meatballRecipe = screen.getByTestId('recipe-2');
+  expect(within(meatballRecipe).getByText('meatballs')).toBeVisible();
+});
