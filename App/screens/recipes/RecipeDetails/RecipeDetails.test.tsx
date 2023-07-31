c08dc1673c976e8b18d5c3d51dd8e211c2201421
edwilson <edward.e.j.wilson@gmail.com>
2023-07-31 20:43:00 +0100

    Add a unit test for the recipe detail view

---
+++ App/screens/recipes/RecipeDetails/RecipeDetails.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import {render, screen} from '@testing-library/react-native';
+
+import {RecipeDetails} from './RecipeDetails';
+import {useRecipeDetails} from '../../../services/restAPI/requests/recipes';
+
+jest.mock('../../../services/restAPI/requests/recipes');
+
+test('renders recipe details', async () => {
+  // Mock out the recipe details API call
+  const mockRecipeDetails = {
+    data: {
+      id: 1,
+      name: 'sausages',
+      description: 'some description',
+      images: [],
+      created_at: '',
+      updated_at: '',
+    },
+    friendlyErrors: null,
+    isLoading: false as false,
+  };
+  jest.mocked(useRecipeDetails).mockReturnValueOnce(mockRecipeDetails);
+
+  // Provide the necessary route parameters
+  const route = {params: {recipeId: 1}};
+
+  render(<RecipeDetails navigation={jest.fn() as any} route={route as any} />);
+
+  // Sausage recipe should be shown
+  expect(screen.getByText('sausages')).toBeVisible();
+  expect(screen.getByText('some description')).toBeVisible();
+});
