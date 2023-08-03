8040120d87a028314af190c1352af1fcc53344b6
edwilson <edward.e.j.wilson@gmail.com>
2023-08-03 07:32:21 +0100

    Split out the menu header into its own component

---
+++ App/screens/menus/MenuDetails/MenuDetailsHeader.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import {faPencil} from '@fortawesome/free-solid-svg-icons';
+import {StyleSheet, View} from 'react-native';
+
+import {
+  bootstrap,
+  Header1,
+  PressablePrimary,
+  PressablePrimaryIcon,
+  TextStyled,
+} from '../../../components/styled';
+import {MenuDetailsPayload} from '../../../services/restAPI/payloads';
+
+type MenuDetailsHeaderProps = {
+  menu: MenuDetailsPayload | null;
+};
+
+export default function MenuDetailsHeader({menu}: MenuDetailsHeaderProps) {
+  return (
+    <View style={[styles.headerContainer, bootstrap.px5]}>
+      <Header1 style={[bootstrap.my3]} testID={'menus-header'}>
+        {menu?.name}
+      </Header1>
+      <TextStyled style={[bootstrap.my3]}>{menu?.description}</TextStyled>
+      <View style={styles.buttonContainer}>
+        <PressablePrimary text={'add meals'} />
+        <PressablePrimaryIcon icon={faPencil} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    // Display
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'space-around',
+  },
+  buttonContainer: {
+    // Display
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '50%',
+  },
+});
--- App/screens/menus/MenuDetails/MenuDetailsView.tsx
+++ App/screens/menus/MenuDetails/MenuDetailsView.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 
-import {faPencil} from '@fortawesome/free-solid-svg-icons';
-import {StyleSheet, FlatList, View} from 'react-native';
+import {FlatList, View} from 'react-native';
 
+import MenuDetailsHeader from './MenuDetailsHeader';
 import MenuItem from './MenuItem';
 import {MenusTopBackground} from '../../../components/images/local';
 import LoadingSpinner from '../../../components/LoadingSpinner';
-import {bootstrap, PressablePrimaryIcon} from '../../../components/styled';
-import {
-  Header1,
-  PressablePrimary,
-  TextStyled,
-} from '../../../components/styled';
+import {bootstrap} from '../../../components/styled';
 import {MenuDetailsPayload} from '../../../services/restAPI/payloads';
 
 type MenuDetailsViewProps = {
@@ -25,49 +20,19 @@ export default function MenuDetailsView({
 }: MenuDetailsViewProps) {
   /** Presentational component showing the detail for a single menu. */
   return (
-    <View style={styles.screenContainer}>
+    <View style={[bootstrap.flex1]}>
       <MenusTopBackground />
       {isLoading ? (
         <LoadingSpinner size={'large'} />
       ) : (
-        <>
-          <View style={[styles.headerContainer, bootstrap.px5]}>
-            <Header1 style={[bootstrap.my3]} testID={'menus-header'}>
-              {menu?.name}
-            </Header1>
-            <TextStyled style={[bootstrap.my3]}>{menu?.description}</TextStyled>
-            <View style={styles.buttonContainer}>
-              <PressablePrimary text={'add meals'} />
-              <PressablePrimaryIcon icon={faPencil} />
-            </View>
-          </View>
-          <FlatList
-            data={menu?.items}
-            renderItem={({item}) => <MenuItem menuItem={item} />}
-            keyExtractor={item => `${item.id}`}
-            refreshing={isLoading}
-          />
-        </>
+        <FlatList
+          data={menu?.items}
+          ListHeaderComponent={<MenuDetailsHeader menu={menu} />}
+          renderItem={({item}) => <MenuItem menuItem={item} />}
+          keyExtractor={item => `${item.id}`}
+          refreshing={isLoading}
+        />
       )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  screenContainer: {
-    flex: 1,
-  },
-  headerContainer: {
-    // Display
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    justifyContent: 'space-around',
-  },
-  buttonContainer: {
-    // Display
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '50%',
-  },
-});
