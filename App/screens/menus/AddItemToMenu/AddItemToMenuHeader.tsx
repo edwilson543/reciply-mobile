aa493d104d1c9241dc1d158988c6fc95db42f724
edwilson <edward.e.j.wilson@gmail.com>
2023-08-04 06:58:38 +0100

    Split out add item to menus header into its own component to avoid having nested scroll view

---
+++ App/screens/menus/AddItemToMenu/AddItemToMenuHeader.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+
+import {StyleSheet, View} from 'react-native';
+
+import SelectDay from './SelectDay';
+import {bootstrap, TextStyled} from '../../../components/styled';
+import {Day} from '../../../services/restAPI/constants';
+import {MenuDetailsPayload} from '../../../services/restAPI/payloads';
+import RecipeListRow from '../../recipes/MyRecipeList/RecipeListRow';
+
+type AddItemToMenuHeaderProps = {
+  menu: MenuDetailsPayload;
+  activeDay: Day;
+  onPressDay: (day: Day) => void;
+};
+
+export default function AddItemToMenuHeader({
+  menu,
+  activeDay,
+  onPressDay,
+}: AddItemToMenuHeaderProps) {
+  const currentItems = menu?.items.filter(item => item.day === activeDay);
+  const activeRecipe = currentItems && currentItems[0]?.recipe;
+
+  return (
+    <>
+      <View style={styles.daysContainer}>
+        <SelectDay
+          day={Day.Monday}
+          isActive={activeDay === Day.Monday}
+          onPress={() => onPressDay(Day.Monday)}
+        />
+        <SelectDay
+          day={Day.Tuesday}
+          isActive={activeDay === Day.Tuesday}
+          onPress={() => onPressDay(Day.Tuesday)}
+        />
+        <SelectDay
+          day={Day.Wednesday}
+          isActive={activeDay === Day.Wednesday}
+          onPress={() => onPressDay(Day.Wednesday)}
+        />
+        <SelectDay
+          day={Day.Thursday}
+          isActive={activeDay === Day.Thursday}
+          onPress={() => onPressDay(Day.Thursday)}
+        />
+        <SelectDay
+          day={Day.Friday}
+          isActive={activeDay === Day.Friday}
+          onPress={() => onPressDay(Day.Friday)}
+        />
+        <SelectDay
+          day={Day.Saturday}
+          isActive={activeDay === Day.Saturday}
+          onPress={() => onPressDay(Day.Saturday)}
+        />
+        <SelectDay
+          day={Day.Sunday}
+          isActive={activeDay === Day.Sunday}
+          onPress={() => onPressDay(Day.Sunday)}
+        />
+      </View>
+      <View style={bootstrap.my3}>
+        <TextStyled>current recipe:</TextStyled>
+        {activeRecipe ? (
+          <RecipeListRow recipe={activeRecipe} />
+        ) : (
+          <TextStyled>-</TextStyled>
+        )}
+      </View>
+      <TextStyled>select recipe:</TextStyled>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  daysContainer: {
+    // Display
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    flexWrap: 'wrap',
+    rowGap: 5,
+    marginVertical: 5,
+  },
+});
--- App/screens/menus/AddItemToMenu/AddItemToMenuView.tsx
+++ App/screens/menus/AddItemToMenu/AddItemToMenuView.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-import {ScrollView, StyleSheet, View, FlatList} from 'react-native';
+import {View, FlatList} from 'react-native';
 
-import SelectDay from './SelectDay';
+import AddItemToMenuHeader from './AddItemToMenuHeader';
 import LoadingSpinner from '../../../components/LoadingSpinner';
-import {bootstrap, TextStyled} from '../../../components/styled';
-import {ScreenName} from '../../../navigation/constants';
+import {TextStyled} from '../../../components/styled';
 import {Day} from '../../../services/restAPI/constants';
 import {
   MenuDetailsPayload,
@@ -31,78 +30,25 @@ export default function AddItemToMenuView({
   activeDay,
   onPressDay,
 }: AddItemToMenuViewProps) {
-  const currentItems = menu?.items.filter(item => item.day === activeDay);
-  const activeRecipe = currentItems && currentItems[0]?.recipe;
-
   return (
     <MenuScreenTemplate>
-      <ScrollView>
-        <View style={styles.daysContainer}>
-          <SelectDay
-            day={Day.Monday}
-            isActive={activeDay === Day.Monday}
-            onPress={() => onPressDay(Day.Monday)}
+      <View>
+        {isLoading && menu ? (
+          <LoadingSpinner />
+        ) : (
+          <FlatList
+            data={suggestedRecipes}
+            renderItem={({item}) => <RecipeListRow recipe={item} />}
+            ListHeaderComponent={
+              <AddItemToMenuHeader
+                menu={menu}
+                activeDay={activeDay}
+                onPressDay={onPressDay}
+              />
+            }
           />
-          <SelectDay
-            day={Day.Tuesday}
-            isActive={activeDay === Day.Tuesday}
-            onPress={() => onPressDay(Day.Tuesday)}
-          />
-          <SelectDay
-            day={Day.Wednesday}
-            isActive={activeDay === Day.Wednesday}
-            onPress={() => onPressDay(Day.Wednesday)}
-          />
-          <SelectDay
-            day={Day.Thursday}
-            isActive={activeDay === Day.Thursday}
-            onPress={() => onPressDay(Day.Thursday)}
-          />
-          <SelectDay
-            day={Day.Friday}
-            isActive={activeDay === Day.Friday}
-            onPress={() => onPressDay(Day.Friday)}
-          />
-          <SelectDay
-            day={Day.Saturday}
-            isActive={activeDay === Day.Saturday}
-            onPress={() => onPressDay(Day.Saturday)}
-          />
-          <SelectDay
-            day={Day.Sunday}
-            isActive={activeDay === Day.Sunday}
-            onPress={() => onPressDay(Day.Sunday)}
-          />
-        </View>
-        <View style={bootstrap.my3}>
-          <TextStyled>current recipe:</TextStyled>
-          {activeRecipe ? (
-            <RecipeListRow recipe={activeRecipe} />
-          ) : (
-            <TextStyled>-</TextStyled>
-          )}
-          <TextStyled>select recipe:</TextStyled>
-          {isLoading ? (
-            <LoadingSpinner />
-          ) : (
-            <FlatList
-              data={suggestedRecipes}
-              renderItem={({item}) => <RecipeListRow recipe={item} />}
-            />
-          )}
-        </View>
-      </ScrollView>
+        )}
+      </View>
     </MenuScreenTemplate>
   );
 }
-
-const styles = StyleSheet.create({
-  daysContainer: {
-    // Display
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    flexWrap: 'wrap',
-    rowGap: 5,
-    marginVertical: 5,
-  },
-});
