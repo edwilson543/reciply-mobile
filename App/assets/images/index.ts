0885ef3ebf5ae2f7dc170e8f93fcda29e5ca4af0
edwilson <edward.e.j.wilson@gmail.com>
2023-08-04 19:57:35 +0100

    Change recipe thumbnail to be a knife fork

--- App/assets/images/index.ts
+++ App/assets/images/index.ts
@@ -1,4 +1,4 @@
-export const RecipeThumbnailPlaceholder = require('./placeholders/recipe-thumbnail.jpg');
+export const RecipeThumbnailPlaceholder = require('./placeholders/recipe-thumbnail.png');
 export const LargeSquareImagePlaceholder = require('./placeholders/recipe-detail.jpg');
 export const LoginRegisterBackgroundImage = require('./background/login-register.jpeg');
 export const RecipesTopBackgroundImage = require('./top-background/spoons-and-butter.jpeg');
