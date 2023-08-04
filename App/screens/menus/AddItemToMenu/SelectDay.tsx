da5c0fd3dd71873f80f4f636338c6ac1c2445fe5
edwilson <edward.e.j.wilson@gmail.com>
2023-08-04 19:16:42 +0100

    Show question mark for days which do not have a recipe

--- App/screens/menus/AddItemToMenu/SelectDay.tsx
+++ App/screens/menus/AddItemToMenu/SelectDay.tsx
@@ -1,64 +1,92 @@
 import React from 'react';
 
-import {faCheck} from '@fortawesome/free-solid-svg-icons';
+import {faCheck, faQuestion} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {View} from 'react-native';
 import {StyleSheet} from 'react-native';
 
 import {PressablePrimary, PressableSecondary} from '../../../components/styled';
 import {Day} from '../../../services/restAPI/constants';
+import {ColourScheme, useColourScheme} from '../../../styles/colourScheme';
 
 type SelectDayProps = {
   day: Day;
   isActive: boolean;
+  hasRecipe: boolean;
   onPress: () => void;
 };
 
-export default function SelectDay({day, isActive, onPress}: SelectDayProps) {
+export default function SelectDay({
+  day,
+  isActive,
+  hasRecipe,
+  onPress,
+}: SelectDayProps) {
+  const colourScheme = useColourScheme();
+  const styleSheet = styles(colourScheme);
+
   const Pressable = isActive ? PressablePrimary : PressableSecondary;
+
+  console.log('Day: ', day, 'has recipe:', hasRecipe);
 
   return (
     <View>
       <Pressable
         onPress={onPress}
         text={Day[day].toLowerCase().slice(0, 3)}
-        style={styles.button}
+        style={styleSheet.button}
       />
-      {/*TODO -> only show tick when recipe selected*/}
-      <View style={styles.checkMarkContainer}>
-        <FontAwesomeIcon icon={faCheck} style={styles.checkMark} />
-      </View>
+      {hasRecipe ? (
+        <View style={[styleSheet.iconContainer, styleSheet.checkIconContainer]}>
+          <FontAwesomeIcon
+            icon={faCheck}
+            style={styleSheet.checkIcon}
+            size={12}
+          />
+        </View>
+      ) : (
+        <View
+          style={[styleSheet.iconContainer, styleSheet.questionIconContainer]}>
+          <FontAwesomeIcon
+            icon={faQuestion}
+            style={styleSheet.questionIcon}
+            size={12}
+          />
+        </View>
+      )}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  button: {
-    // Display
-    width: 75,
-    height: 50,
-    marginVertical: 10,
-    marginHorizontal: 5,
-    padding: 5,
-    // Border
-    borderRadius: 20,
-  },
-  checkMarkContainer: {
-    // Positioning
-    position: 'relative',
-    bottom: 65,
-    left: 60,
-    // Display
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 20,
-    height: 20,
-    // Background and border
-    backgroundColor: '#6ba96b',
-    borderRadius: 10,
-  },
-  checkMark: {
-    // Background and border
-    color: '#fff',
-  },
-});
+const styles = (colourScheme: ColourScheme) =>
+  StyleSheet.create({
+    button: {
+      // Display
+      width: 75,
+      height: 50,
+      marginTop: 20,
+      marginHorizontal: 5,
+      padding: 5,
+      // Border
+      borderRadius: 20,
+    },
+    iconContainer: {
+      // Positioning
+      position: 'relative',
+      bottom: 55,
+      left: 60,
+      // Display
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: 20,
+      height: 20,
+      // Background and border
+      borderRadius: 10,
+      borderWidth: 1,
+      borderColor: colourScheme.fontSecondary,
+    },
+    checkIconContainer: {backgroundColor: colourScheme.alertSuccess},
+    checkIcon: {color: colourScheme.fontSecondary},
+    questionIconContainer: {backgroundColor: colourScheme.alertWarning},
+    questionIcon: {color: colourScheme.fontSecondary},
+  });
